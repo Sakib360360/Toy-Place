9aba1f67543ffc21c56b81c32cbf3a87c434be8a
Sakib360360 <saakibabrar@gmail.com>
2023-05-19 21:12:07 +0600

    delete from client side imidiately

--- src/Components/MyToys/MyToys.jsx
+++ src/Components/MyToys/MyToys.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import MyToysCard from './MyToysCard';
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const {user} = useContext(AuthContext)
@@ -13,13 +14,52 @@ const MyToys = () => {
         })
     },[])
 
-    
+    const handleDelete = (id) => {
+
+
+        Swal.fire({
+            title: 'Are you sure?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            denyButtonText: `No`,
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                fetch(`https://toy-place-server.vercel.app/delete/${id}`, {
+                    method: "DELETE",
+                    headers: {
+                        'Content-Type': 'application/json',
+
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if(data.deletedCount>0){
+                            Swal.fire('Deleted!', '', 'success')
+                            const newMyToys = myToys.filter(toy=>toy._id!==id)
+                            setMyToys(newMyToys)
+                        }
+                    })
+                
+            } else if (result.isDenied) {
+                Swal.fire('Try again', '', 'info')
+            }
+        })
+
+
+
+
+
+
+    }
     console.log(myToys)
     return (
         <div className='max-w-6xl'>
             <div className='flex justify-center flex-wrap gap-8'>
                 {
-                    myToys.map((toy,index)=><MyToysCard key={index} toy={toy}></MyToysCard>)
+                    myToys.map((toy,index)=><MyToysCard handleDelete={handleDelete} key={index} toy={toy}></MyToysCard>)
                 }
             </div>
         </div>
--- src/Components/MyToys/MyToysCard.jsx
+++ src/Components/MyToys/MyToysCard.jsx
@@ -1,46 +1,10 @@
 import React from 'react';
-import Swal from 'sweetalert2';
 
-const MyToysCard = ({ toy }) => {
+
+const MyToysCard = ({ toy,handleDelete }) => {
     const { price, name, category, postedBy, details, picture, _id } = toy
-    const handleDelete = (id) => {
-
-
-        Swal.fire({
-            title: 'Are you sure?',
-            showDenyButton: true,
-            showCancelButton: true,
-            confirmButtonText: 'Yes',
-            denyButtonText: `No`,
-        }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
-            if (result.isConfirmed) {
-                fetch(`https://toy-place-server.vercel.app/delete/${_id}`, {
-                    method: "DELETE",
-                    headers: {
-                        'Content-Type': 'application/json',
-
-                    }
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data)
-                        if(data.deletedCount>0){
-                            Swal.fire('Deleted!', '', 'success')
-                        }
-                    })
-                
-            } else if (result.isDenied) {
-                Swal.fire('Try again', '', 'info')
-            }
-        })
-
-
-
-
-
-
-    }
+    
+    
     return (
         <div className="card card-side bg-base-100 shadow-xl max-w-xs">
             <figure><img src={picture} alt="Movie" /></figure>
